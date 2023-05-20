6cf4ecfc2f3ae4f8c6d42807ec5221c5ac9fcc66
thechaos16 <thechaos16@gmail.com>
2023-05-20 22:59:56 +0900

    update 263th problem

---
+++ src/0263_ugly_number.rs
@@ -0,0 +1,20 @@
+fn main () {
+    let input = 6;
+    println!("{}", is_ugly(input));
+    let input = 14;
+    println!("{}", is_ugly(input));
+}
+
+fn is_ugly(n: i32) -> bool {
+    let mut res = n;
+    while res % 2 == 0 {
+        res /= 2;
+    }
+    while res % 3 == 0 {
+        res /= 3;
+    }
+    while res % 5 == 0 {
+        res /= 5;
+    }
+    return res == 1;
+}
