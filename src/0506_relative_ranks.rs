45a75b529a18ce571a32e5568155d6b1b27e3127
thechaos16 <thechaos16@gmail.com>
2023-09-25 22:41:07 +0900

    better code

--- src/0506_relative_ranks.rs
+++ src/0506_relative_ranks.rs
@@ -1,30 +1,21 @@
-use std::collections::HashMap;
-
-
 fn main() {
     let score = vec![5, 4, 3, 2, 1, 99, 12, 23];
     println!("{:?}", find_relative_ranks(score));
 }
 
 fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
-    let mut score_ = score.clone();
-    score_.sort_by(|a, b| b.cmp(a));
-    let mut hashmap = HashMap::new();
-    for idx in 0..score_.len() {
-        hashmap.insert(score_[idx], convert_name(idx + 1));
-    }
-    let mut result = vec![];
-    for idx in 0..score.len() {
-        result.push(hashmap.get(&score[idx]).unwrap().to_string());
-    }
-    return result;
-}
+    let mut sorted_indices: Vec<usize> = (0..score.len()).collect();
+    sorted_indices.sort_by_key(|&idx| -score[idx]);
 
-fn convert_name(rank: usize) -> String {
-    match rank {
-        1 => return "Gold Medal".to_string(),
-        2 => return "Silver Medal".to_string(),
-        3 => return "Bronze Medal".to_string(),
-        _ => return format!("{}", rank)
+    let mut result = vec![String::new(); score.len()];
+    for (rank, &idx) in sorted_indices.iter().enumerate() {
+        result[idx] = match rank {
+            0 => "Gold Medal".to_string(),
+            1 => "Silver Medal".to_string(),
+            2 => "Bronze Medal".to_string(),
+            _ => (rank + 1).to_string(),
+        };
     }
+
+    result
 }
