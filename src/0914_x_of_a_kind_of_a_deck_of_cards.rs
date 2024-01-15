995258ebf5f51fda8133a71348e62d731a299e9b
thechaos16 <thechaos16@gmail.com>
2024-01-15 22:48:53 +0900

    solve

---
+++ src/0914_x_of_a_kind_of_a_deck_of_cards.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = vec![1, 2, 3, 4, 4, 3, 1, 2];
+    println!("{}", has_groups_size_x(input));
+}
+
+fn has_groups_size_x(deck: Vec<i32>) -> bool {
+    if deck.len() == 1 {
+        return false;
+    }
+    let mut hashmap = HashMap::new();
+    for card in deck {
+        let before_cnt = hashmap.entry(card).or_insert(0);
+        *before_cnt += 1;
+    }
+    let counter_vec: Vec<(&i32, &u32)> = hashmap.iter().collect();
+    for idx in 0..counter_vec.len() {
+        for idx2 in (idx + 1)..counter_vec.len() {
+            if counter_vec[idx].1 >= counter_vec[idx2].1 {
+                if gcd(*counter_vec[idx].1 as i32, *counter_vec[idx2].1 as i32) == 1 {
+                    return false;
+                }
+            } else {
+                if gcd(*counter_vec[idx2].1 as i32, *counter_vec[idx].1 as i32) == 1 {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+fn gcd(m: i32, n: i32) -> i32 {
+    if m % n == 0 {
+        return n;
+    } else {
+        return gcd(n, m % n);
+    }
+}
--- src/1018_binary_prefix_divisible_by_5.rs
+++ src/1018_binary_prefix_divisible_by_5.rs
@@ -5,10 +5,11 @@ fn main() {
 
 fn prefixes_div_by5(nums: Vec<i32>) -> Vec<bool> {
     let mut res = vec![];
-    let mut cur_num = 0 as i64;
+    let mut cur_num = 0;
     for num in nums {
         cur_num *= 2;
-        cur_num += num as i64;
+        cur_num += num;
+        cur_num %= 5;
         res.push(cur_num % 5 == 0);
     }
     return res;
