07a8bff6fab3c26046ff6a810fcb1013324f36a3
thechaos16 <thechaos16@gmail.com>
2024-10-14 22:06:13 +0900

    2530 solution

---
+++ src/2530_maximal_score_after_applying_k_operations.rs
@@ -0,0 +1,19 @@
+use std::collections::BinaryHeap;
+
+
+fn main() {
+    let nums = vec![10, 10, 10, 10, 10];
+    let k = 5;
+    println!("{}", max_kelements(nums, k));
+}
+
+fn max_kelements(nums: Vec<i32>, k: i32) -> i64 {
+    let mut heap = BinaryHeap::from(nums);
+    let mut ans = 0 as i64;
+    for _ in 0..k {
+        let cur = heap.pop().unwrap();
+        ans += cur as i64;
+        heap.push((cur as f32 / 3.0).ceil() as i32);
+    }
+    return ans;
+}
