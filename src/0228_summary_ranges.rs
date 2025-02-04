efb66fe5bfe06caddd1cffe58be2883a7b25517c
thechaos16 <thechaos16@gmail.com>
2025-02-04 23:15:47 +0900

    update summary range

--- src/0228_summary_ranges.rs
+++ src/0228_summary_ranges.rs
@@ -7,26 +7,30 @@ fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
     if nums.len() == 0 {
         return vec![];
     }
-    let mut start = nums[0];
-    let mut end = nums[0];
     let mut ans = vec![];
-    for idx in 1..nums.len() {
-        if nums[idx] == end + 1 {
-            end = nums[idx];
-        } else {
-            if start == end {
-                ans.push(format!("{}", start));
+    let mut iter = nums.into_iter().peekable();
+    if let Some(mut start) = iter.next() {
+        let mut end = start;
+        while let Some(&next) = iter.peek() {
+            if next == end + 1 {
+                end = next;
+                iter.next();
             } else {
-                ans.push(format!("{}->{}", start, end));
+                ans.push(format_range(start, end));
+                start = next;
+                end = next;
+                iter.next();
             }
-            start = nums[idx];
-            end = nums[idx];
         }
-    }
-    if start == end {
-        ans.push(format!("{}", start));
-    } else {
-        ans.push(format!("{}->{}", start, end));
+        ans.push(format_range(start, end));
     }
     return ans;
+}
+
+fn format_range(start: i32, end: i32) -> String {
+    if start == end {
+        return start.to_string();
+    } else {
+        return format!("{}->{}", start, end);
+    }
 }
