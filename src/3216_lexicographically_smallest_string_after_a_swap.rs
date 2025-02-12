18dbf45e1f5f7982f80b10351631cc94939af6ad
thechaos16 <thechaos16@gmail.com>
2025-02-12 22:10:06 +0900

    fix error

--- src/3216_lexicographically_smallest_string_after_a_swap.rs
+++ src/3216_lexicographically_smallest_string_after_a_swap.rs
@@ -6,15 +6,11 @@ fn main() {
 fn get_smallest_string(s: String) -> String {
     let mut chars: Vec<_> = s.chars().collect();
     for idx in 0..(chars.len() - 1) {
-        if chars[idx] > chars[idx + 1] {
-            if idx == 0 && chars[idx + 1] == '0' {
-                continue;
-            } else {
-                let temp = chars[idx + 1];
-                chars[idx + 1] = chars[idx];
-                chars[idx] = temp;
-                break;
-            }
+        if chars[idx] > chars[idx + 1] && (chars[idx] as i32) % 2 == (chars[idx + 1] as i32) % 2 {
+            let temp = chars[idx + 1];
+            chars[idx + 1] = chars[idx];
+            chars[idx] = temp;
+            break;
         }
     }
     return chars.iter().collect();
