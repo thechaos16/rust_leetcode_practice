723272a30634a25ef0195e726aeeaa0c29dda414
thechaos16 <thechaos16@gmail.com>
2025-04-03 23:35:35 +0900

    update

--- src/0860_lemonade_change.rs
+++ src/0860_lemonade_change.rs
@@ -1,37 +1,34 @@
-use std::cmp::min;
-
-
 fn main() {
     let bills = vec![5, 5, 5, 5, 10, 20];
     println!("{}", lemonade_change(bills));
 }
 
 fn lemonade_change(bills: Vec<i32>) -> bool {
-    let mut cur_change = vec![0, 0, 0];
+    let mut available_bills = [0, 0];
     for bill in bills {
-        if bill == 5 {
-            cur_change[2] += 1;
-        } else if bill == 10 {
-            cur_change[1] += 1;
-        } else {
-            cur_change[0] += 1;
-        }
-        let mut change = bill - 5;
-        if change == 0 {
-            continue;
-        }
-        let twenty = min(cur_change[0], change / 20);
-        cur_change[0] -= twenty;
-        change -= twenty * 20;
-        let ten = min(cur_change[1], change / 10);
-        cur_change[1] -= ten;
-        change -= ten * 10;
-        let five = min(cur_change[2], change / 5);
-        cur_change[2] -= five;
-        change -= five * 5;
-        if change > 0 {
-            return false;
+        match bill {
+            5 => {
+                available_bills[0] += 1;
+            },
+            10 => {
+                if available_bills[0] == 0 {
+                    return false;
+                }
+                available_bills[0] -= 1;
+                available_bills[1] += 1;
+            },
+            20 => {
+                if available_bills[0] >= 1 && available_bills[1] >= 1 {
+                    available_bills[0] -= 1;
+                    available_bills[1] -= 1;
+                } else if available_bills[0] >= 3 {
+                    available_bills[0] -= 3;
+                } else {
+                    return false;
+                }
+            },
+            _ => panic!("Invalid bill demoniation {}", bill),
         }
     }
-    return true;
+    true
 }
