54fc0e509781b0c5a8304252dc4311a135b7136d
thechaos16 <thechaos16@gmail.com>
2025-04-07 22:36:56 +0900

    better rust code for 0412

--- src/0412_fizzbuzz.rs
+++ src/0412_fizzbuzz.rs
@@ -4,17 +4,10 @@ fn main () {
 }
 
 fn fizz_buzz(n: i32) -> Vec<String> {
-    let mut res: Vec<String> = vec![];
-    for idx in 1..n + 1 {
-        if idx % 3 == 0 && idx % 5 == 0 {
-            res.push("FizzBuzz".to_string());
-        } else if idx % 3 == 0 {
-            res.push("Fizz".to_string());
-        } else if idx % 5 == 0 {
-            res.push("Buzz".to_string());
-        } else {
-            res.push(idx.to_string());
-        }
-    }
-    return res;
+    (1..=n).map(|num| match (num % 3 == 0, num % 5 == 0){
+        (true, true) => "FizzBuzz".to_string(),
+        (true, false) => "Fizz".to_string(),
+        (false, true) => "Buzz".to_string(),
+        _ => num.to_string(),
+    }).collect()
 }
