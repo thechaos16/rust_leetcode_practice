084a4b114bd1d55a13cc333875065b8e0e75a177
thechaos16 <thechaos16@gmail.com>
2025-06-23 23:45:53 +0900

    upgrade 2129

--- src/2129_capitalize_the_title.rs
+++ src/2129_capitalize_the_title.rs
@@ -4,21 +4,15 @@ fn main() {
 }
 
 fn capitalize_title(title: String) -> String {
-    let words = title.split(" ").collect::<Vec<_>>();
-    let mut new_string = "".to_string();
-    for (word_idx, &word) in words.iter().enumerate() {
-        for idx in 0..word.len() {
-            let ch = word.chars().nth(idx).unwrap();
-            if idx == 0 && word.len() > 2 {
-                new_string.push_str(&ch.to_uppercase().to_string());
-            } else {
-                new_string.push_str(&ch.to_lowercase().to_string());
+    title.split_whitespace().map(|word| {
+        if word.len() > 2 {
+            let mut chars = word.chars();
+            match chars.next() {
+                Some(first) => first.to_uppercase().collect::<String>() + &chars.as_str().to_lowercase(),
+                None => String::new(),
             }
+        } else {
+            word.to_lowercase()
         }
-        if word_idx == words.len() - 1 {
-            break;
-        }
-        new_string.push_str(&" ".to_string());
-    }
-    return new_string;
+    }).collect::<Vec<_>>().join(" ")
 }
